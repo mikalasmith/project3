524368fd7c73aae740d57e94c958e01aafa4f6b7
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-08-07 18:56:32 -0400

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
+import Footer from "/.component/Footer";
 
 class App extends Component {
   render() {
     return (
+      <div>
         <Nav />
+        <Footer />
+      </div>
     );
   }
 }
