7844c2e94b4159d0531382e8cda6e88218afbdee
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-08-08 10:59:55 -0400

    work on routers

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import volunteerHands from "./img/volunteerHands.png";
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => (
     <div>
@@ -9,22 +10,19 @@ const Nav = () => (
         <img src="/assets/img/volunteerHands.png"></img>
         <a href="/">Helping Hands</a>
     </div>*/}
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white sticky-top navbar">
-        <img src={volunteerHands} id="bgimg"></img>
-    <h5 className="my-0 mr-md-auto font-weight-normal" id="site-name"><a href="/">Pay it Forward</a></h5>
+    <img src={ volunteerHands }></img>
+    <h5 className="my-0 mr-md-auto font-weight-normal" id="site-name">Pay it Forward</h5>
     <nav className="my-2 my-md-0 mr-md-3">
-      <a className="p-2 text-dark" href="/">home
+      <Link className="p-2 text-dark" to="/">home
         <span className="sr-only">(current)</span>
-        </a>
-      {/*THIS WILL NEED TO BE FOR IF SIGNED IN BUT UNSURE HOW TO DO WITH REACT RIGHT NOW*/}
-      {/*<a class="p-2 text-dark" href="/profile">profile</a>
-        <a class="p-2 text-dark" href="/opportunities">opportunities</a>
-        <a class="btn btn-outline-primary" href="/logout">log out</a>*/}
-    <a className="p-2 text-dark" href="/singup">sign up</a>
+        </Link>
+        <Link className="p-2 text-dark" to="/profile">profile</Link>
+        <Link className="p-2 text-dark" to="/allopportunities">opportunities</Link>
+       {/*} <a class="btn btn-outline-primary" href="/logout">log out</a>*/}
+        <Link className="p-2 text-dark" to="/signup">sign up</Link>
     </nav>
-        <a className="btn btn-outline-primary" href="/singin">log in</a>
-   </div> 
-   </div>
+    {/*<a className="btn btn-outline-primary" href="/singin">log in</a>*/}
+</div> 
 );
 
 export default Nav;
