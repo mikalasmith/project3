f7bf86a21647ebed1d39088cdd84a2a4a836c5f0
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-08-08 12:34:15 -0400

    more routers

---
+++ client/src/pages/VolunteerProfile/VolunteerProfile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./VolunteerProfile.css";
+
+const VolunteerProfile = props => (
+ <p>NEED TO MAKE AND USE COMPONETS TO RENDER THE VOLUNTEER PROFILE HERE</p>
+);
+
+export default VolunteerProfile;
